7459511ada77f914ec4d45aab705fbfe8b22ff2c
jadielson <jajadielsonmanoel@gmail.com>
2022-12-12 17:22:41 -0300

    User criado e configurado com o prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Padaria Blue')
+    .setDescription('API responsavel pela gestão da Padaria da Blue')
+    .setVersion('1.0.0')
+    .addTag('users')
+    .addTag('products')
+    .addTag('status')
+    .addTag('categories')
+    .addTag('orders')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/user/users.controller.ts
@@ -0,0 +1,66 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+  } from '@nestjs/common';
+  import { ApiOperation, ApiTags } from '@nestjs/swagger';
+  import { User } from './entity/user.entity';
+  import { CreateUserDto } from './dto/create-user.dto';
+  import { UpdateUserDto } from './dto/updated-user.dto';
+  import { UsersService } from './users.service';
+  
+  @ApiTags('users')
+  @Controller('users')
+  export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+  
+    @Post()
+    @ApiOperation({
+      summary: 'Cria um novo usuário',
+    })
+    create(@Body() dto: CreateUserDto): Promise<User | void> {
+      return this.usersService.create(dto);
+    }
+  
+    @Get()
+    @ApiOperation({
+      summary: 'Lista de todos usuários',
+    })
+    findAll(): Promise<User[]> {
+      return this.usersService.findAll();
+    }
+  
+    @Get(':id')
+    @ApiOperation({
+      summary: 'Lista usuário por id',
+    })
+    findOne(@Param('id') id: string): Promise<User> {
+      return this.usersService.findOne(id);
+    }
+  
+    
+  
+    @Patch(':id')
+    @ApiOperation({
+      summary: 'Atualizar um usuário',
+    })
+    update(
+      @Param('id') id: string,
+      @Body() dto: UpdateUserDto,
+    ): Promise<User | void> {
+      return this.usersService.update(id, dto);
+    }
+  
+    @Delete(':id')
+    @ApiOperation({
+      summary: 'Deletar usuário',
+    })
+    remove(@Param('id') id: string): Promise<User> {
+      return this.usersService.remove(id);
+    }
+  }
+  
